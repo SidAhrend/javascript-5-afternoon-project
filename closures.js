ebc5dca581e8ee94e58b9646ef17e12c34492776
Sid Ahrend <sidahrend57@gmail.com>
2019-10-17 15:29:08 -0600

    finished

--- closures.js
+++ closures.js
@@ -24,11 +24,13 @@ function outer() {
   
 // Code Here
 
+let inner = outer();
 
 
 //Once you do that, invoke inner.
 
 //Code Here
+outer();
 
 
 
@@ -52,7 +54,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
+let callJake = callFriend('Jake');
+callJake('435-555-9248')
 
 
 ////////// PROBLEM 3 //////////
@@ -65,12 +68,18 @@ function callFriend(name) {
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+function makeCounter(){
+  let count = 0;
+  function addOne(){
+    return count += 1;
+  }
+  return addOne
+}
+
+// count(); // 1
+// count(); // 2
+// count(); // 3
+// count(); // 4
 
 
 
@@ -87,9 +96,15 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
   return {
-
+    inc: function(){
+      value = value +1;
+      return value;
+    },
+    dec: function(){
+      value = value -1;
+      return value;
+    }
   };
 }
 
@@ -113,9 +128,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  let message =function(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,6 +162,9 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function(){
+      return privateMethod();
+    }
   };
 })();
 
@@ -163,6 +184,12 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num){
+      return secret += num;
+    },
+    takeAwayFromSecret: function(num){
+      return secret -= num;
+    }
   };
 }
 
@@ -188,9 +215,12 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    function count(i){
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
   }
+  count(i)
+}
 }
 timeOutCounter();
